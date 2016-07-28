1e2e66739ddf259b9c59c7e8dfaee7a90a13b31f
Greg Smith <crashingwaves@fea.st>
2016-07-28 17:24:28 -0600

    Add tests for position, getter/setters

--- test/spec/test.js
+++ test/spec/test.js
@@ -2,13 +2,19 @@
   'use strict';
 
   describe('Map should be properly created', function () {
-    var height = 500, width = 500;
-    var margin = { top: 50, right: 60, bottom: 70, left: 80 };
-    beforeAll(function() {
-      var testMap = strikeMap();
-      testMap.width(width).height(height).margin(margin);
-      d3.select('#chart').call(testMap);
+
+    beforeEach(function() {
+      this.height = 500;
+      this.width = 500;
+      this.margin = { top: 50, right: 60, bottom: 70, left: 80 };
+      this.testMap = strikeMap();
+      this.testMap.width(this.width).height(this.height).margin(this.margin);
+      d3.select('#chart').call(this.testMap);
     });
+
+    afterEach(function() {
+      d3.select('#chart').html("");
+    })
 
     describe('the svg container', function() {
       it('should be created', function() {
@@ -16,16 +22,55 @@
       });
 
       it('should be the specified width', function() {
-        expect(+selectSVG().attr('width')).toBe(width + margin.right + margin.left);
+        expect(+selectSVG().attr('width')).toBe(this.width + this.margin.right + this.margin.left);
       });
 
       it('should be the specified height', function() {
-        expect(+selectSVG().attr('height')).toBe(height + margin.top + margin.bottom);
+        expect(+selectSVG().attr('height')).toBe(this.height + this.margin.top + this.margin.bottom);
       });
-    })
+    });
+
+    describe('the map object', function() {
+      it('should be positioned inside the SVG container by the margin values', function() {
+        expect(selectMapObject().attr('transform')).toBe(`translate(${this.margin.left},${this.margin.top})`)
+      });
+      describe('has getter methods', function() {
+        it('should return the current width', function() {
+          expect(this.testMap.width()).toBe(this.width);
+        });
+        it('should return the current height', function() {
+          expect(this.testMap.height()).toBe(this.height);
+        });
+        it('should return the current margin object', function() {
+          expect(this.testMap.margin()).toEqual(this.margin);
+        });
+      });
+      describe('has setter methods', function() {
+        it('should set the width', function() {
+          expect(this.testMap.width()).toBe(this.width);
+          this.testMap.width(1000);
+          expect(this.testMap.width()).toBe(1000);
+        });
+        it('should set the height', function() {
+          expect(this.testMap.height()).toBe(this.height);
+          this.testMap.height(1000);
+          expect(this.testMap.height()).toBe(1000);
+        });
+        it('should set the margin object', function() {
+          var newMargin = { top: 80, right: 70, bottom: 60, left: 50 }
+          expect(this.testMap.margin()).toBe(this.margin);
+          this.testMap.margin(newMargin);
+          expect(this.testMap.margin()).toEqual(newMargin);
+        });
+      });
+    });
 
     function selectSVG() {
       return d3.select('svg');
+    }
+
+    function selectMapObject() {
+      return d3.select('g.mapContainer');
     }
   });
 })();
