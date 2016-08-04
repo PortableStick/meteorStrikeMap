2937a19ee72bd49b73efed13a1677945e0db9475
Greg Smith <crashingwaves@fea.st>
2016-08-04 17:21:47 -0600

    Fix impact and color scales

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ function strikeMap() {
       initialRotation = 60,
       maximumScaleFactor = 10,
       lastTranslation = [0,0],
-      impactRadiusMax = 40,
       impactRadiusMin = 4,
+      impactRadiusMax = 50,
       lastScale = null,
       margin = { top: 0, right: 0, bottom: 0, left: 0 },
       width = 1680 - margin.left - margin.right,
       height = 800 - margin.top - margin.bottom;
+
+  var waterColor = '#447DCC',
+      landColor = '#FFF7C0';
   var projection = d3.geo.mercator()
                       .rotate([initialRotation, 0])
                       .scale(0.85)
                       .translate([width / 2, height / 2]);
-  var impactScale = d3.scale.quantile().range([impactRadiusMin, 8, 10, 16, 20, impactRadiusMax]);
-  var colorScale = d3.scale.category10();
+  var impactScale = d3.scale.log().range([impactRadiusMin, 8, 15, impactRadiusMax]);
+  var colorScale = d3.scale.log().range(['#005C1B', '#00D800', '#F56F61', '#A81000']);
 
   var bounds = findMercatorBounds(projection, maximumLatitude),
       scale =  width / (bounds[1][0] - bounds[0][0]), // ratio of svg width to boundary width
@@ -51,7 +54,7 @@ function strikeMap() {
         .call(zoom);
         svg.append('rect')
           .attr({
-            'fill': '#266D98',
+            'fill': waterColor,
             'width': width,
             'height': height
           })
@@ -59,7 +62,7 @@ function strikeMap() {
       var meteorites = data[1].features;
       var massData = meteorites.map(function(d){return +d.properties.mass || 1;});
       var median = d3.median(massData);
-      var inputDomain = [d3.extent(massData)[0], median, median * 2, median * 4, d3.extent(massData)[1]];
+      var inputDomain = [d3.extent(massData)[0], median * 5, median * 50, d3.extent(massData)[1]];
       impactScale.domain(inputDomain);
       colorScale.domain(inputDomain);
 
@@ -72,7 +75,7 @@ function strikeMap() {
             .attr({
               'd': path,
               'stroke-width': 1,
-              'fill': '#95E1D3'
+              'fill': landColor
             });
       var meteorites = svg.append('g')
         .selectAll('path')
@@ -93,13 +96,13 @@ function strikeMap() {
           'fill': function(d) {
             return setHue(d.properties.mass);
           },
-          'fill-opacity': 0.5,
+          'fill-opacity': 0.6,
           'stroke': 1,
           'stroke-fill': 'white'
         })
         .on('mouseover', function(d) {
           tooltip.style({
-            opacity: 0.8,
+            opacity: 1,
             'top': function() { return `${d3.event.pageY - 50}px`; },
             'left': function() { return `${d3.event.pageX + 30}px`; }
           })
@@ -119,9 +122,9 @@ function strikeMap() {
 
   function formatMass(mass) {
     mass = +mass;
-    if (mass > 1000000) {
+    if (mass >= 1000000) {
       return `${mass.toFixed(1)/1000000} ${mass === 1 ? 'megagram' : 'megagrams'}`;
-    } else if(mass > 1000) {
+    } else if(mass >= 1000) {
       return `${mass.toFixed(1)/1000} ${mass === 1 ? 'kilogram' : 'kilograms'}`;
     } else {
       return `${mass} ${mass === 1 ? 'gram' : 'grams'}`;
